4d50ccb31bc32b80d1d2ded1e8ca16a912c7cbd7
Julian Gomez <juliane.gomez@upb.edu.co>
2021-03-01 13:06:52 -0500

    Ejemplo Variables

---
+++ Variables.java
@@ -0,0 +1,18 @@
+public class Variables
+{ 
+    public static void main(String[] args) 
+    {
+        byte dia;
+        byte a, b, c;
+        int d;
+
+        dia = 127;
+        a = 60;
+        b = 40;
+
+        d = a+b;
+
+        System.out.println("La suma es " + d);
+    }
+
+}
